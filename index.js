805a5956ed075319cfee70b674586d3a7d4144c4
Nitin Singh <nitinsinghji12@gmail.com>
2025-08-03 13:53:43 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+import 'dotenv/config'
+
+import express from 'express';
+
+const app = express();
+
+const port = 4000;
+
+app.get("/", (req, res) => {
+	res.send("Hello World!");
+});
+
+app.get('/twitter', (req,res) => {
+  res.send("Welcome to Twitter")
+});
+
+app.get('/login', (req,res) => {
+  res.send("<h1> Please Login to Continue")
+})
+
+app.listen(process.env.PORT, () => {
+	console.log(`Example app listening on port ${port}`);
+});
